ce0edc9e08ed4eb06b7517f5351a709bb66bc3fa
mohamedhossam <wikohossam35@gmail.com>
2022-03-05 00:52:13 +0200

    update userSeeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,9 +9,6 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
