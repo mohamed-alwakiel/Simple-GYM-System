a5ff736cb75f66be88140b25ea0f04454eb1e2ed
Ahmed-bastwesy <ahmed.reda.bastawesy@gmail.com>
2022-03-04 23:15:47 +0200

    user update profile api

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateUserRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Resources\UserResource;
+use Illuminate\Validation\Rule;
+
 class UserController extends Controller
 {
     public function index()
@@ -13,4 +16,28 @@ class UserController extends Controller
         $users = User::where('role_id', 4)->get();
         return   UserResource::collection($users);
     }
+
+    public function update(Request $request, $id)
+    {
+        $user=User::find($id);
+        $data=$request->validate([
+            'name' => 'required|string|min:3',
+            'email' => ['required','email',Rule::unique('users')->ignore($user->id)],
+            'date_of_birth' => 'required|date',
+            'national_id' => ['required','digits_between:14,14',Rule::unique('users')->ignore($user->id)],
+        ]);
+        $user->update([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'national_id' => $data['national_id'],
+            'date_of_birth' =>$data['date_of_birth'],
+        ]);
+        $response=[
+            'message'=>'data updated successfully',
+            'user'=>$user,
+        ];
+        return response($response);
+
+    }
+
 }
