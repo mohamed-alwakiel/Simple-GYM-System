9b626ef26de44af24a7c6a9b8fc6a2d7b91566f4
Ahmed-bastwesy <ahmed.reda.bastawesy@gmail.com>
2022-03-04 22:31:00 +0200

    user login&register api

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Resources\UserResource;
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::where('role_id', 4)->get();
+        return   UserResource::collection($users);
+    }
+}
