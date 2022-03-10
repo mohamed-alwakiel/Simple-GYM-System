5f8ffd6bff692dfc09add2fa1df5439c1c87b841
NermeenShehab <nermeenshehab490@gmail.com>
2022-03-10 23:52:50 +0200

    add client role on bought package

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -379,7 +379,7 @@
                             </a>
                         </li>
                         
-                        @hasanyrole('gymManager|cityManager|admin')
+                        @hasanyrole('gymManager|cityManager|admin|client')
                         <li class="nav-item">
                             <a href="{{ route('buyPackage.index') }}" class="nav-link">
                                 <i class="nav-icon fas fa-money-check"></i>
