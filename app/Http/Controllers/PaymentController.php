9f3f86c0ffa5cb7f1a175cbe7d9145a7d916a943
NermeenShehab <nermeenshehab490@gmail.com>
2022-03-11 19:22:13 +0200

    add valiation on payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -42,9 +42,9 @@ class PaymentController extends Controller
         $package_id = $requestObj->package_id;
         $city = $requestObj->city;
 
-        // if ($gym_id == null ||  $user_id == null ||  $package_id == null  || $city == null) {
-        //     return Redirect::back()->withErrors(['message' => 'complete your data']);
-        // } else {
+        if ($gym_id == null ||  $user_id == null ||  $package_id == null  || $city == null) {
+            return Redirect::back()->withErrors(['message' => 'complete your data']);
+        } else {
 
             Stripe::create([
                 'price' => $package->price,
